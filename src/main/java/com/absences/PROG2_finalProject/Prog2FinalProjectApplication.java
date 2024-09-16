9b9a338184df15b3959ca1f9c6c439464e385ab4
Kef <hei.kephas@gmail.com>
2024-09-16 11:10:02 +0300

    first commit

---
+++ src/main/java/com/absences/PROG2_finalProject/Prog2FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.absences.PROG2_finalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog2FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog2FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/absences/PROG2_finalProject/Prog2FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absences.PROG2_finalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog2FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
